d6e1c3677b7b8c7a2d35491b1cbc60da9de515c5
Cem Fidan <devcfidan@gmail.com>
2023-06-11 15:26:01 +0300

    car-add component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RentalComponent } from './components/rental/rental.component';
 import { CarComponent } from './components/car/car.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { CarAddComponent } from './components/car-add/car-add.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: TodoComponent },
   { path: 'cars', component: CarComponent },
   { path: 'cars/brand/:brandId', component: CarComponent },
   { path: 'cars/color/:colorId', component: CarComponent },
+  { path: 'cars/add', component: CarAddComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
@@ -17,6 +17,7 @@ import { UserComponent } from './components/user/user.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { CarAddComponent } from './components/car-add/car-add.component';
 
 @NgModule({
   declarations: [
@@ -30,13 +31,15 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     UserComponent,
     TodoComponent,
     FilterPipePipe,
-    CartSummaryComponent
+    CartSummaryComponent,
+    CarAddComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({positionClass:"toast-bottom-right"})
   ],
